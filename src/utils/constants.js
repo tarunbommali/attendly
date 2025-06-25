e585589bdff340ab83df8966ed07942a21fb910e
Tarun <btaruntej143@gmail.com>
2025-06-25 23:01:38 +0530

    date update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const PORTFOLIO_URL = "https://tarunbommali.netlify.app/";
 
 export const xslsheetURL = "https://sheetdb.io/api/v1/xoyajaelp4f6k"
 
-export const lastUpdateOn = "APRIL 2025"
+export const lastUpdateOn = "JUNE 2025"
